f9aafb6a0e417decf06d7fda4655a01d80dfc585
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 18:22:40 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan1/perintahmenampilkanvaluekelayar/PBO310117121Latihan1PerintahMenampilkanValueKeLayar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan1.perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMKE
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan data diri
+ */
+public class PBO310117121Latihan1PerintahMenampilkanValueKeLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rifqi Mujahid");
+        System.out.println("ALAMAT\t\t: Tubagus");
+        System.out.println("TEMPAT LAHIR\t\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t\t: 11 Juli 1999");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: nonton film barat");
+        System.out.println("MAKES\t\t: miayam");
+        System.out.println("MIKES\t\t: kuah miayam");
+    }
+    
+}
